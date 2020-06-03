8b7b827b4dd86557119664a473cfd3e7c8bec3de
Yakiv Yereskovskyi <yakiv.yereskovskyi@gmail.com>
2020-06-03 13:58:52 +0300

    add unit format

--- coder/play/src/main/scala/poppet/coder/play/instances/PlayCoderInstances.scala
+++ coder/play/src/main/scala/poppet/coder/play/instances/PlayCoderInstances.scala
@@ -1,5 +1,8 @@
 package poppet.coder.play.instances
 
+import play.api.libs.json.Format
+import play.api.libs.json.JsObject
+import play.api.libs.json.JsSuccess
 import play.api.libs.json.JsValue
 import play.api.libs.json.Json
 import play.api.libs.json.Reads
@@ -14,4 +17,5 @@ trait PlayCoderInstances {
         a => Future.successful(coder(a))
     implicit def writesToCoder[A](implicit writes: Writes[A]): Coder[A, JsValue] = writes.writes(_)
     implicit def readsToCoder[A](implicit reads: Reads[A]): Coder[JsValue, A] = reads.reads(_).asEither.right.get
+    implicit val unitFormat = Format[Unit](_ => JsSuccess(()), _ => JsObject(Seq.empty))
 }
