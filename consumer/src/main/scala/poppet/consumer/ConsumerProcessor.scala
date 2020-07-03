84e00edd824570a3374a12e22b53b63813883aad
Yakiv Yereskovskyi <yakiv.yereskovskyi@gmail.com>
2020-07-03 16:34:35 +0300

    fix consumer processor generation

--- consumer/src/main/scala/poppet/consumer/ConsumerProcessor.scala
+++ consumer/src/main/scala/poppet/consumer/ConsumerProcessor.scala
@@ -38,7 +38,7 @@ object ConsumerProcessor {
                         q"""$fmonad.flatMap(${codedArgument(h)})((${Ident(h.name)}: ${h.typeSignature}) => $tree)"""
                     case hs => q"""$fmonad.flatten(
                         _root_.cats.Semigroupal.${TermName("map" + hs.size)}(..${hs.map(codedArgument)})(
-                            ..${hs.map(h => q"${Ident(h.name)}: ${h.typeSignature}")} => $tree
+                            ..${hs.map(h => q"${Ident(h.name)}: $itype")} => $tree
                         )
                     )"""
                 }
--- consumer/src/test/scala/ConsumerTester.scala
+++ consumer/src/test/scala/ConsumerTester.scala
@@ -1,6 +1,7 @@
 import cats.implicits._
 import poppet.coder.Coder
 import poppet.consumer.ConsumerProcessor
+import poppet.dto.Request
 import poppet.dto.Response
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits._
@@ -8,23 +9,22 @@ import scala.concurrent.Future
 import scala.concurrent.duration.Duration
 
 object ConsumerTester {
-    val a = 29
+    val a = 31
     trait A {
         def a0: Int
         def a1(): Int
-        def a2(b: String): Int
-        def a3(b1: String, b2: String): Int
-        def a4(b1: String, b2: String)(b3: String): Int
+        def a2(b: Long): Int
+        def a3(b1: Long, b2: Long): Int
+        def a4(b1: Long, b2: Long)(b3: Long): Int
     }
     def main(args: Array[String]): Unit = {
         implicit val c1 = new Coder[Future[String], Int] {
-            override def apply(a: Future[String]): Int = Await.result(a, Duration.Inf).toInt
+            override def apply(a: Future[String]): Int = ???
         }
-        println(ConsumerProcessor.apply[A].generate[String, Future]().f(
-            r => {
-                println(r)
-                Future.successful(Response("1"))
-            }
-        ).a4("", "")(""))
+        implicit val c2 = new Coder[Long, Future[String]] {
+            override def apply(a: Long): Future[String] = ???
+        }
+
+        println(ConsumerProcessor.apply[A].generate[String, Future]())
     }
 }
